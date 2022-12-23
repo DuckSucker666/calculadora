072af87dcf3150b1329538adb6b8417da8e5449a
DuckSucker666 <luisalcaloide.105@gmail.com>
2022-12-23 17:11:30 -0500

    calculadora/angularmaterial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calculadora';
+  a! : number;
+  b! : number;
+
+  sumar!: boolean;
+  restar!: boolean;
+  multiplicar!: boolean;
+  dividir!: boolean;
+  resultado!: number;
+
+  enviar() {
+    if (this.sumar === true){
+      this.resultado = this.a + this.b;
+    } else if (this.restar === true){
+      this.resultado = this.a - this.b;
+    } else if (this.multiplicar === true){
+      this.resultado = this.a * this.b;
+    } else if (this.dividir === true) {
+      this.resultado = this.a / this.b;
+    }
+  }
+
+
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { FormsModule } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatIconModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatButtonModule
+  ],
+  exports: [
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSlideToggleModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
